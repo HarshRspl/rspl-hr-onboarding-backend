b19fb663adb0bd2de178b7dc1189e7632afbf85f
HarshRspl <panditharsh15@gmail.com>
2026-02-26 15:41:54 +0530

    Update CandidateRepository.java

--- src/main/java/com/rspl/onboarding/repo/CandidateRepository.java
+++ src/main/java/com/rspl/onboarding/repo/CandidateRepository.java
@@ -2,9 +2,11 @@ package com.rspl.onboarding.repo;
 
 import com.rspl.onboarding.domain.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-  Optional<Candidate> findByEmailIdIgnoreCase(String emailId);
+    Optional<Candidate> findByOnboardingToken(String token);
+    Optional<Candidate> findByEmail(String email);
 }
