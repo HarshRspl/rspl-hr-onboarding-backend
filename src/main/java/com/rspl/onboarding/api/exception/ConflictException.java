28dd3cb1beac84f8b8f3cebf575bea6c28101fe6
HarshRspl <panditharsh15@gmail.com>
2026-02-26 15:57:46 +0530

    Create ConflictException.java

---
+++ src/main/java/com/rspl/onboarding/api/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.rspl.onboarding.api.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
