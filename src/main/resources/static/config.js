3a345b880529d72dd2871c457ddc250ddd3fc66c
HarshRspl <panditharsh15@gmail.com>
2026-03-01 18:06:36 +0530

    Create config.js

---
+++ src/main/resources/static/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  API_BASE: 'https://rspl-hr-onboarding-backend-production.up.railway.app/api',
+  APP_BASE: 'https://rspl-hr-onboarding-backend-production.up.railway.app',
+  ENV: 'production'
+};
